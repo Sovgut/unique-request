c1a861b2c404f70d1316851b957a0303a5e55250
Sergey Sovgut <sergey.sovgut@outlook.com>
2021-01-25 19:18:32 +0200

    Changed method for getting ip address

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const express = require("express")
 
 express()
   .get("/", (request, response) => {
-    console.log(request.ip)
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress
+    console.log(ip)
 
-    response.send(response.ip)
+    response.send(ip)
   })
   .listen(process.env.PORT, console.log(`Server started on ${process.env.PORT}`))
