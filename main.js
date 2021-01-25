1b2ddc0d92f05dc470f2ab7998e45f33c9b54421
Sergey Sovgut <sergey.sovgut@outlook.com>
2021-01-25 19:27:36 +0200

    I think this is final version

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const express = require("express")
 
+const uniqueKeys = []
+
 express()
   .get("/", (request, response) => {
     const ip = request.headers["x-forwarded-for"] || request.connection.remoteAddress
-    console.log(ip)
+    const userAgent = request.headers["user-agent"]
+    const uniqueKey = Buffer.from(`${ip}-${userAgent}`).toString("base64")
 
-    response.send(ip)
+    if (uniqueKeys.includes(uniqueKey)) {
+      return response.send(`<h3>I know you</h3><p>Your name is: ${uniqueKey}</p>`)
+    }
+
+    uniqueKeys.push(uniqueKey)
+    return response.send(`<h3>Hello, are you new?</h3><p>I gave you a new name: ${uniqueKey}</p>`)
   })
   .listen(process.env.PORT, console.log(`Server started on ${process.env.PORT}`))
