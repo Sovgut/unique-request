0a2afe33df745e3af061970e514b420b1054b821
Sergey Sovgut <sergey.sovgut@outlook.com>
2021-01-25 19:14:18 +0200

    changed output from ips to ip

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const express = require("express")
 
 express()
   .get("/", (request, response) => {
-    console.log(request.ips)
+    console.log(request.ip)
 
-    response.send(response.ips)
+    response.send(response.ip)
   })
   .listen(process.env.PORT, console.log(`Server started on ${process.env.PORT}`))
