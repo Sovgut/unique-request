e6a6f7b494aa23fc349789ee08930d6ab67c00ea
Sergey Sovgut <sergey.sovgut@outlook.com>
2021-01-25 20:11:05 +0200

    Removed any dependencies (raw nodejs)

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-const express = require("express")
-
+const http = require("http")
 const uniqueKeys = []
 
-express()
-  .get("/", (request, response) => {
+http
+  .createServer((request, response) => {
     const ip = request.headers["x-forwarded-for"] || request.connection.remoteAddress
     const userAgent = request.headers["user-agent"]
     const uniqueKey = Buffer.from(`${ip}-${userAgent}`).toString("base64")
 
     if (uniqueKeys.includes(uniqueKey)) {
-      return response.send(`<h3>I know you</h3><p>Your name is: ${uniqueKey}</p>`)
+      return response.write(`<h3>I know you</h3><p>Your name is: ${uniqueKey}</p>`) && response.end()
     }
 
     uniqueKeys.push(uniqueKey)
-    return response.send(`<h3>Hello, are you new?</h3><p>I gave you a new name: ${uniqueKey}</p>`)
+    return response.write(`<h3>Hello, are you new?</h3><p>I gave you a new name: ${uniqueKey}</p>`) && response.end()
   })
-  .listen(process.env.PORT, console.log(`Server started on ${process.env.PORT}`))
+  .listen(process.env.PORT || 3030, () => console.log("Server was started"))
